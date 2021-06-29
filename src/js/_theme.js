d1dc54e8d6784821ad696176fe1a10f0461816f9
sergey280680 <sergey280680h@gmail.com>
2021-06-29 12:00:29 +0300

    при первичной загрузке на боди добавляется светлая тема

--- src/js/_theme.js
+++ src/js/_theme.js
@@ -3,23 +3,19 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+const body = document.querySelector('body');
 const themeToggle = document.querySelector('#theme-switch-toggle');
 
-// проверка на наличие класса темной или свелой темы у body 
-// если не присутствует ни одна из тем то добавляем светлую тему
-if (document.body.classList.contains(Theme.LIGHT || Theme.DARK)) {
-  return;
-}
-document.body.classList.add(Theme.LIGHT);
-
 if (localStorage.getItem('theme') === Theme.DARK) {
-  document.body.classList.add(Theme.DARK);
+  body.classList.add(Theme.DARK);
   themeToggle.checked = true;
+} else {
+  body.classList.add(Theme.LIGHT);
 }
 
 function changeThemeBody(addClass, removeClass) {
-  document.body.classList.remove(removeClass);
-  document.body.classList.add(addClass);
+  body.classList.remove(removeClass);
+  body.classList.add(addClass);
   localStorage.setItem('theme', addClass);
 }
 
